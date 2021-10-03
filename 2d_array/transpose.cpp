294262390c81d01301f011768e746792d9557573
Somnath Prasad <somnathprasad559@gmail.com>
2021-10-03 17:02:31 +0530

    spiral print and transpose

---
+++ 2d_array/transpose.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j && i>j)
+            {
+                swap(a[i][j], a[j][i]);
+            }
+        }
+    }
+    cout<<"\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout<<a[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
